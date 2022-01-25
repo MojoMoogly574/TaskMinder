78b3d9e0c4ba09b9f06a9f881d2027cd92ff68e9
Mojo <mojo@nuralab.com>
2022-01-24 22:40:55 -0600

    Initial Commit

---
+++ TaskMinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskMinder
+//
+//  Created by Joseph DeWeese on 1/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskMinder/TaskMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMinderApp.swift
+//  TaskMinder
+//
+//  Created by Joseph DeWeese on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
